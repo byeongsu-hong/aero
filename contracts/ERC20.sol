5e2ab2f913502f203195957a64e75f27499d3ec9
Vista <retail3210@gmail.com>
2018-09-12 16:48:57 +0900

    Deposit contracts implementation (WIP)

---
+++ contracts/ERC20.sol
@@ -0,0 +1,76 @@
+pragma solidity ^0.4.24;
+
+import "./SafeMath.sol";
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address who) external view returns (uint256);
+
+    function allowance(address owner, address spender)
+        external view returns (uint256);
+
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function approve(address spender, uint256 value)
+        external returns (bool);
+
+    function transferFrom(address from, address to, uint256 value)
+        external returns (bool);
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+
+/**
+ * @title SafeERC20
+ * @dev Wrappers around ERC20 operations that throw on failure.
+ * To use this library you can add a `using SafeERC20 for ERC20;` statement to your contract,
+ * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
+ */
+library SafeERC20 {
+    function safeTransfer(
+        IERC20 token,
+        address to,
+        uint256 value
+    )
+        internal
+    {
+        require(token.transfer(to, value));
+    }
+
+    function safeTransferFrom(
+        IERC20 token,
+        address from,
+        address to,
+        uint256 value
+    )
+        internal
+    {
+        require(token.transferFrom(from, to, value));
+    }
+
+    function safeApprove(
+        IERC20 token,
+        address spender,
+        uint256 value
+    )
+        internal
+    {
+        require(token.approve(spender, value));
+    }
+}
---
+++ contracts/ParentChain.sol
@@ -0,0 +1,120 @@
+pragma solidity ^0.4.24;
+
+import "./SafeMath.sol";
+import "./MerkleProof.sol";
+import "./ECDSA.sol";
+import "./ERC20.sol";
+
+/**
+ * @title ParentChain
+ * @dev A gateway contract on the parent chain, bridging parent chain with the child chain.
+ */
+contract ParentChain {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    event Deposit(
+        address indexed depositor,
+        uint256 indexed depositBlockNumber,
+        address token,
+        uint256 amount
+    );
+
+    event BlockSubmit(
+        bytes32 root,
+        uint256 timestamp
+    );
+
+    struct Exit {
+        address owner;
+        address token;
+        uint256 amount;
+    }
+
+    struct ChildBlock {
+        bytes32 root;
+        uint256 timestamp;
+    }
+
+    // constraints from Plasma MVP by OmiseGO
+    uint256 public constant CHILD_BLOCK_INTERVAL = 1000;
+
+    address public operator;
+
+    uint256 public currentChildBlock;
+    uint256 public currentDepositBlock;
+    uint256 public currentFeeExit;
+
+    mapping (uint256 => ChildBlock) public childBlocks;
+    mapping (uint256 => Exit) public exits;
+    mapping (address => address) public exitsQueues;
+
+    modifier onlyOperator() { require(msg.sender == operator, "Access Denied."); _;}
+
+    constructor() public {
+        operator = msg.sender;
+    }
+
+    /**
+     * @dev Deposit ERC20 token into the child chain.
+     */
+    function deposit(
+        address from,
+        IERC20 token,
+        uint256 value)
+        public
+    {
+        require(
+            currentDepositBlock < CHILD_BLOCK_INTERVAL, 
+            "Only allow up to 1000 deposits per child block.");
+        
+        token.safeTransferFrom(msg.sender, address(this), value);
+
+        // create a deposit block
+        bytes memory data = abi.encodePacked(msg.sender, address(token), value);
+        bytes32 root = keccak256(data);
+        
+        uint256 depositBlock = getNextDepositBlockIndex();
+        childBlocks[depositBlock] = ChildBlock({
+            root: root,
+            timestamp: block.timestamp
+        });
+        currentDepositBlock = currentDepositBlock.add(1);
+
+        emit Deposit(msg.sender, depositBlock, address(token), value);
+    }
+
+    /**
+     * @dev Submit a merkle root of child chain blocks, by the operator.
+     */
+    function submitBlock(bytes32 _root) public onlyOperator {
+        childBlocks[currentChildBlock] = ChildBlock({
+            root: _root,
+            timestamp: block.timestamp
+        });
+
+        // update index
+        currentChildBlock = currentChildBlock.add(CHILD_BLOCK_INTERVAL);
+        currentDepositBlock = 1;
+        
+        emit BlockSubmit(_root, block.timestamp);
+    }
+
+    /**
+     * @dev Get block of the child chain.
+     */
+    function getChildBlock(uint256 _blockNumber) 
+        public 
+        view
+        returns (bytes32, uint256) {
+        return (childBlocks[_blockNumber].root, childBlocks[_blockNumber].timestamp);
+    }
+
+    function getNextDepositBlockIndex()
+        public
+        view
+        returns (uint256)
+    {
+        return currentChildBlock.sub(CHILD_BLOCK_INTERVAL).add(currentDepositBlock);
+    }
+}
