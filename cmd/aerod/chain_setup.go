dd7f094f557a6b845d52a872e6e109b21ee43b4a
Vista <retail3210@gmail.com>
2018-09-13 16:48:52 +0900

    aerod: Better genesis block generation.

--- cmd/aerod/chain_setup.go
+++ cmd/aerod/chain_setup.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/cmd/utils"
 	ethutils "github.com/ethereum/go-ethereum/cmd/utils"
@@ -21,6 +22,10 @@ import (
 	"os"
 )
 
+type GenesisConfig struct {
+	Genesis core.Genesis `json:"genesis"`
+}
+
 func defaultNodeConfig() *node.Config {
 	cfg := node.DefaultConfig
 	cfg.Name = clientName
@@ -34,6 +39,7 @@ func defaultNodeConfig() *node.Config {
 func developerGenesisBlock(period uint64, faucet common.Address) *core.Genesis {
 	config := *params.AllCliqueProtocolChanges
 	config.Clique.Period = period
+	config.ChainID = big.NewInt(defaultNetworkId)
 
 	genesis := &core.Genesis{
 		Config:     &config,
@@ -49,35 +55,24 @@ func developerGenesisBlock(period uint64, faucet common.Address) *core.Genesis {
 			common.BytesToAddress([]byte{6}): {Balance: big.NewInt(1)}, // ECAdd
 			common.BytesToAddress([]byte{7}): {Balance: big.NewInt(1)}, // ECScalarMul
 			common.BytesToAddress([]byte{8}): {Balance: big.NewInt(1)}, // ECPairing
-			faucet: {Balance: new(big.Int).Sub(new(big.Int).Lsh(big.NewInt(1), 256), big.NewInt(9))},
+			faucet:                           {Balance: new(big.Int).Sub(new(big.Int).Lsh(big.NewInt(1), 256), big.NewInt(9))},
 		},
 	}
 	return genesis
 }
 
-
 func loadGenesis(genesisPath string) *core.Genesis {
-	if len(genesisPath) == 0 {
-		log.Println("No genesis block data is given. Using default...")
-		key, _ := account.NewKey()
-		genesis := developerGenesisBlock(defaultBlockPeriod, key.Address)
-
-		log.Println("Auto-generated genesis faucet address : ", key.Address.Hex())
-		log.Println(" ↳ Private Key : ", hexutil.Encode(crypto.FromECDSA(key.PrivateKey)))
-		return genesis
-	}
-
 	file, err := os.Open(genesisPath)
 	if err != nil {
 		utils.Fatalf("Failed to read genesis file: %v", err)
 	}
 	defer file.Close()
 
-	genesis := new(core.Genesis)
+	genesis := new(GenesisConfig)
 	if err := json.NewDecoder(file).Decode(genesis); err != nil {
 		utils.Fatalf("invalid genesis file: %v", err)
 	}
-	return genesis
+	return &genesis.Genesis
 }
 
 func makeDatabaseHandles() int {
@@ -98,11 +93,7 @@ func makeDatabaseHandles() int {
 }
 
 func setupNode(ctx *cli.Context) (*node.Node, error) {
-	genesisPath := ctx.Args().First()
-	genesis := loadGenesis(genesisPath)
-
 	nodeConfig := defaultNodeConfig()
-	nodeConfig.P2P.ListenAddr = ":17600"
 	nodeConfig.DataDir = ""
 
 	// node access config
@@ -116,12 +107,39 @@ func setupNode(ctx *cli.Context) (*node.Node, error) {
 		return nil, fmt.Errorf("Failed to create the protocol stack: %v", err)
 	}
 
-	// unlock account manager
 	ks := stack.AccountManager().Backends(keystore.KeyStoreType)[0].(*keystore.KeyStore)
-	faucet, err := ks.NewAccount("")
-	if err != nil {
-		return nil, fmt.Errorf("Failed to create account: %v", err)
+
+	var genesis *core.Genesis
+	var faucet accounts.Account
+	genesisPath := ctx.String("genesis")
+
+	if len(genesisPath) == 0 {
+		// create new genesis block
+		log.Println("No genesis block data is given. Using default...")
+		key, _ := account.NewKey()
+		genesis = developerGenesisBlock(defaultBlockPeriod, key.Address)
+
+		log.Println("Auto-generated faucet address : ", key.Address.Hex())
+		log.Println(" ↳ Private Key : ", hexutil.Encode(crypto.FromECDSA(key.PrivateKey)))
+
+		// make generated account default
+		faucet, err = ks.ImportECDSA(key.PrivateKey, "")
+		if err != nil {
+			return nil, fmt.Errorf("Failed to import account: %v", err)
+		}
+
+	} else {
+		// load genesis.json, created by puppeth
+		genesis = loadGenesis(genesisPath)
+
+		// create new default account
+		faucet, err = ks.NewAccount("")
+		if err != nil {
+			return nil, fmt.Errorf("Failed to create account: %v", err)
+		}
 	}
+
+	// unlock the keystore
 	if err := ks.Unlock(faucet, ""); err != nil {
 		return nil, fmt.Errorf("Failed to unlock account: %v", err)
 	}
@@ -132,7 +150,7 @@ func setupNode(ctx *cli.Context) (*node.Node, error) {
 	ethConfig.Genesis = genesis
 	ethConfig.NetworkId = genesis.Config.ChainID.Uint64()
 	ethConfig.EnablePreimageRecording = true
-	ethConfig.MinerGasPrice = big.NewInt(1)
+	ethConfig.MinerGasPrice = big.NewInt(0)
 	ethConfig.DatabaseHandles = makeDatabaseHandles()
 
 	ethutils.RegisterEthService(stack, ethConfig)
--- cmd/aerod/params.go
+++ cmd/aerod/params.go
@@ -1,7 +1,8 @@
 package main
 
 const (
-	clientName = "aero"
-	clientVersion = "0.1.0"
+	clientName         = "aero"
+	clientVersion      = "0.1.0"
+	defaultNetworkId   = 76
 	defaultBlockPeriod = 2
 )
