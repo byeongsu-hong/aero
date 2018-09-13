6c0b0c654633ace0fdffd584ed0efeba5979baed
Vista <retail3210@gmail.com>
2018-09-13 11:29:22 +0900

    common: Testnet launcher

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"github.com/airbloc/ethornge/account"
-	"github.com/airbloc/ethornge/gorange"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
-	"log"
+	"github.com/frostornge/ethornge/account"
+	"github.com/frostornge/ethornge/gorange"
 )
 
 func main() {
-	log.Println("Hello byeongsu")
 	key, _ := account.NewKey()
-	println(string(crypto.FromECDSA(key.PrivateKey)))
-	node, _ := gorange.Launch(
-		gorange.DefaultLocalConfig([]common.Address{key.Address}, 100),
-	)
+	node, err := gorange.Launch(
+		gorange.DefaultLocalConfig([]common.Address{key.Address}, 100))
+	if err != nil {
+		panic(err)
+	}
 	defer node.Stop()
 }
