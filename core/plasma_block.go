505be2c1292c7b365696510cb401b14c416f9046
therne <retail3210@gmail.com>
2018-09-25 20:31:07 +0900

    aero: Build SMT and calculate TxRoot

--- core/plasma_block.go
+++ core/plasma_block.go
@@ -3,19 +3,32 @@ package core
 import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/loomnetwork/mamamerkle"
+	"github.com/pkg/errors"
 	"math/big"
 )
 
 type PlasmaBlock struct {
 	Transactions []*PlasmaTx
 	TxRoot       common.Hash
-	Tree         mamamerkle.SparseMerkleTree
+	TxTree       *mamamerkle.SparseMerkleTree
 
 	Id big.Int
 }
 
-func NewPlasmaBlock(transactions []*PlasmaTx) *PlasmaBlock {
+func NewPlasmaBlock(transactions []*PlasmaTx) (*PlasmaBlock, error) {
+	// build sparse merkle tree and calculate root
+	leaves := make(map[uint64][]byte)
+	for _, tx := range transactions {
+		leaves[tx.SlotId] = tx.Hash.Bytes()
+	}
+
+	tree, err := mamamerkle.NewSparseMerkleTree(64, leaves)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to construct SMT")
+	}
 	return &PlasmaBlock{
 		Transactions: transactions,
-	}
+		TxRoot:       common.BytesToHash(tree.Root()),
+		TxTree:       tree,
+	}, nil
 }
